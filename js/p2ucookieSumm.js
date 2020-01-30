cfe61f7883d614a7be1c6bc2fb60dbfddcfc9eb8
Al Long <al.lonster@gmail.com>
2020-01-30 12:10:56 +0000

    OT1=

---
+++ js/p2ucookieSumm.js
@@ -0,0 +1,34 @@
+function GetCookie(name) {
+    var arg = name + "=";
+    var alen = arg.length;
+    var clen = document.cookie.length;
+    var i = 0;
+    while (i < clen) {
+        var j = i + alen;
+        if (document.cookie.substring(i, j) == arg)
+            return "here";
+        i = document.cookie.indexOf(" ", i) + 1;
+        if (i == 0) break;
+    }
+    return null;
+}
+
+function p2uFirstCookie() {
+    var visit = GetCookie("FirstTimeVisitCookie");
+    if (visit == null) {
+        var expire = new Date();
+        expire = new Date(expire.getTime() + 7776000000);
+        document.cookie = "FirstTimeVisitCookie=here; expires=" + expire;
+        document.getElementById("h1").innerHTML = "Welcome to Pharmacy2U <first time>!";	// New visitor
+    } else {
+        document.getElementById("h1").innerHTML = " Hi, <firstname> we’re now connected to your GP";
+        var bgImageChange = document.querySelector(".p2u-main");// Returning visitor
+        bgImageChange.classList.add("p2u-returning");
+    }
+}
+
+p2uFirstCookie();
+
+
+
+
